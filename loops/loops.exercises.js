ec2a64b818c6a53fd6f2cead7dcb259c8c9854a5
Arsen Tabaku <arsen.tabaku@vrdirect.com>
2021-08-30 23:55:18 +0200

    Loops - Completed exercises 7 to 10

--- loops/loops.exercises.js
+++ loops/loops.exercises.js
@@ -125,36 +125,35 @@ function displayPyramidWithIncreasedNumbersByOne(n) {
     }
 
     for (let k = 0; k < i + 1; k++) {
-      pattern += (number++) + " ";
+      pattern += number++ + " ";
     }
 
     console.log(pattern);
   }
-
 }
 
 console.log("\n6. Pyramid with increased numbers by one: ");
 displayPyramidWithIncreasedNumbersByOne(4);
 
-// 7. Write a program to make such a pattern like a pyramid with a number which will 
+// 7. Write a program to make such a pattern like a pyramid with a number which will
 // repeat the number in the same row.
 //    1
 //   2 2
 //  3 3 3
 // 4 4 4 4
 
-function displayPyramidWithRepeatedNumberPerRow(n) {
+function displayPyramidWithRepeatedNumberPerRow(n) {
   let pattern = "";
   let number = 1;
 
   for (let i = 0; i < n; i++) {
     pattern = "";
 
-    for (let k = 0; k < n-1-i; k++) {
+    for (let k = 0; k < n - 1 - i; k++) {
       pattern += " ";
     }
 
-    for (let j = 0; j < i+1; j++) {
+    for (let j = 0; j < i + 1; j++) {
       pattern += number + " ";
     }
 
@@ -165,3 +164,93 @@ function displayPyramidWithRepeatedNumberPerRow(n) {
 
 console.log("\n7. Pyramid with repeated numbers per row: ");
 displayPyramidWithRepeatedNumberPerRow(4);
+
+// 8. Write a program to display the pattern like a pyramid using asterisk and each
+// row contain an odd number of asterisks.
+//    *
+//   ***
+//  *****
+// *******
+
+function displayPyramidWithOddAsterisks(n) {
+  let pattern = "";
+
+  for (let i = 0; i < n; i++) {
+    pattern = "";
+
+    for (let j = 0; j < n - 1 - i; j++) {
+      pattern += " ";
+    }
+
+    for (let k = 0; k < i * 2 + 1; k++) {
+      pattern += "*";
+    }
+
+    console.log(pattern);
+  }
+}
+
+console.log("\n8. Pyramid with odd asterisks: ");
+displayPyramidWithOddAsterisks(4);
+
+// 9. Write a program to display the sum of the series [ 9 + 99 + 999 + 9999 ...].
+
+function displayNumberOfSeries(n) {
+  let sum = 9;
+  let pattern = 9;
+
+  for (let i = 0; i < n - 1; i++) {
+    pattern = pattern * 10 + 9;
+    sum += pattern;
+  }
+
+  console.log(sum);
+}
+
+console.log("\n9. Sum of series: ");
+displayNumberOfSeries(5);
+
+// 10. Write a program to print the Floyd's Triangle.
+function floydTriangle(n) {
+  let pattern = "";
+
+  for (let i = 0; i < n; i++) {
+    pattern = "";
+
+    for (let j = 0; j < i + 1; j++) {
+      if (i % 2 == 0) {
+        if (j % 2 == 0) {
+          pattern += "1";
+        } else {
+          pattern += "0";
+        }
+      } else if (i % 2 == 1) {
+        if (j % 2 == 0) {
+          pattern += "0";
+        } else {
+          pattern += "1";
+        }
+      }
+    }
+
+    console.log(pattern);
+  }
+}
+
+console.log("\n10. Floyd's Triangle: ");
+floydTriangle(5);
+
+// 11. Write a program to display the sum of the series [ 1+x+x^2/2!+x^3/3!+....].
+
+// 12. Write a program to find the sum of the series [ x - x^3 + x^5 + ......].
+
+// 13. Write a program to display the n terms of square natural number and their sum.
+// 1 4 9 16 ... n
+
+// 14. Write a program to check whether a given number is a perfect number or not.
+
+// 15. Write a program to find the perfect numbers within a given number of range.
+
+// 16. Write a program to check whether a given number is an armstrong number or not.
+
+// 17. Write a program to find the Armstrong number for a given range of number.
